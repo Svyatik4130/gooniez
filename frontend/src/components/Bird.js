8bad2e32106eb51e1215cdb8ed0d98ca83275a57
Svyatik4130 <barberbi1@icloud.com>
2022-03-25 23:20:15 +0100

    sneakerhead

--- frontend/src/components/Bird.js
+++ frontend/src/components/Bird.js
@@ -6,13 +6,13 @@ const Bird = ({ top, gameRunning }) => {
   if (gameRunning) {
     return (
       <div className="Bird" style={style}>
-        <img src="bird.gif" style={{ width: "70px", height: "70px" }} />
+        <img src="bird.gif" style={{ width: "70px", height: "50px", objectFit: "cover" }} />
       </div>
     )
   } else {
     return (
       <div className="Bird" style={style}>
-        <img src="birdStatic.png" style={{ width: "70px", height: "70px" }} />
+        <img src="staticbird.png" style={{ width: "70px", height: "70px" }} />
       </div>
     )
   } 
--- frontend/src/components/ExportScore.js
+++ frontend/src/components/ExportScore.js
@@ -4,12 +4,12 @@ import jwt from 'jsonwebtoken'
 export default function ExportScore({ score, gameRunning }) {
     var token = jwt.sign({ score }, 'vlad')
     const [btnText, setbtnText] = useState("Copy my score to clipboard")
-    const text = `:gg_lfg: FLAPPY GOONIEZ :gg_lfg:\n *I just got ${score} point @SirVaalok | GG | FLAPPY GOON#9067 game* \n **Can you be better**:question: \n\n my security token: ${token}`
+    const text = `:bird: FLAPPY SNEAKERS :hiking_boot: \n *I just got ${score} point at @Un_Like#6483 game* \n **Can you be better**:question: \n\n my security token: ${token}`
 
     if (!gameRunning && score > 0) {
         return (
             <div onClick={() => {
-                navigator.clipboard.writeText(text); setbtnText("COPIED! paste it into hapes chat"); setTimeout(() => {
+                navigator.clipboard.writeText(text); setbtnText("COPIED! paste it into the chat"); setTimeout(() => {
                     setbtnText("Copy my score to clipboard")
                 }, 4000);
             }} className="exp-btn">
